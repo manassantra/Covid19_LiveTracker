3e246593e865759b1d4848342ad7acfd7f74313a
@manassantra <manas.nshmdgp@gmail.com>
2020-05-09 20:54:24 +0530

    mySecondFinalUpdate

---
+++ build/precache-manifest.3fe1dc34a7f28b8bb512e4b55bbdfcf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f125b8a4b6187bfea9a0edf8f1697fe0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "729505cbdc01ba1aacff",
+    "url": "/static/css/main.658ad7a3.chunk.css"
+  },
+  {
+    "revision": "d8882603771fc3751903",
+    "url": "/static/js/2.97549e72.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.97549e72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "729505cbdc01ba1aacff",
+    "url": "/static/js/main.ef7f94e2.chunk.js"
+  },
+  {
+    "revision": "bcb3e891f8c02b6d3b30",
+    "url": "/static/js/runtime-main.2d5c5ce2.js"
+  }
+]);
